cdad9712ccb43f3ffccb5e97a00ca7d9e5f6a352
dk009dk <60841968+dk009dk@users.noreply.github.com>
2021-08-12 09:30:01 +0000

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '88c85601834876f896877fc90ba65286'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '85cca24f-4d37-4c1d-957e-954727a46eb5'
+};
